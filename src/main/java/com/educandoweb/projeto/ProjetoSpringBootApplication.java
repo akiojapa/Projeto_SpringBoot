a0681bff0e8e2de3c57fb6e1e144587d96d21194
akiojapa <akioandrei@hotmail.com>
2022-03-03 08:46:11 -0300

    Resources
    Foi implementado uma classe de usuário e uma classe para utilizar os recursos da classe Usuário no Spring Boot.

---
+++ src/main/java/com/educandoweb/projeto/ProjetoSpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.educandoweb.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringBootApplication.class, args);
+		System.out.println("Oi");
+		System.out.println("Sou o Akio");
+	}
+	
+
+}
---
+++ src/main/java/com/educandoweb/projeto/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educandoweb.projeto.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.projeto.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Akio", "Akio@gmail.com", "44988471531", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
