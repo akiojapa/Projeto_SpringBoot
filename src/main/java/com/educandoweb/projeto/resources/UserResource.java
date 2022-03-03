3a34065281c214184ee5de1b09a5f4f4f034e499
akiojapa <akioandrei@hotmail.com>
2022-03-03 14:48:41 -0300

    Service Layer, component registration
    Implementado uma classe de serviços de usuário que facilita o acesso aos dados de cada Id.

--- src/main/java/com/educandoweb/projeto/resources/UserResource.java
+++ src/main/java/com/educandoweb/projeto/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.educandoweb.projeto.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.projeto.entities.User;
+import com.educandoweb.projeto.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Akio", "Akio@gmail.com", "44988471531", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findByid(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
