0c26c837c738ac4aba944110be2513cbe6641b03
akiojapa <akioandrei@hotmail.com>
2022-03-04 17:09:21 -0300

    Entity Product
    Foi implementado uma classe Produto que é interligado com usuários e seus pedidos.

--- src/main/java/com/educandoweb/projeto/config/TestConfig.java
+++ src/main/java/com/educandoweb/projeto/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.projeto.entities.Category;
 import com.educandoweb.projeto.entities.Order;
+import com.educandoweb.projeto.entities.Product;
 import com.educandoweb.projeto.entities.User;
 import com.educandoweb.projeto.entities.enums.OrderStatus;
 import com.educandoweb.projeto.repositories.CategoryRepository;
 import com.educandoweb.projeto.repositories.OrderRepository;
+import com.educandoweb.projeto.repositories.ProductRepository;
 import com.educandoweb.projeto.repositories.UserRepository;
 
 @Configuration
@@ -28,10 +30,29 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private CategoryRepository categoryRepository;
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		
+		
 		User u1 = new User(null, "Maikola Snow", "maikedasmeninas@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Akio Andrei", "akio@gmail.com", "988471531", "123456");
 		
@@ -39,13 +60,12 @@ public class TestConfig implements CommandLineRunner {
 		Order o2 = new Order(null, Instant.parse("2022-03-02T03:42:10Z"),OrderStatus.WAITTING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2022-03-03T15:21:22Z"),OrderStatus.DELIVERED, u1);
 		
-		Category cat1 = new Category(null, "Electronics");
-		Category cat2 = new Category(null, "Books");
-		Category cat3 = new Category(null, "Computers");
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		
+		
+		
 	}
 
 }
--- src/main/java/com/educandoweb/projeto/services/OrderService.java
+++ src/main/java/com/educandoweb/projeto/services/OrderService.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.projeto.entities.Order;
-import com.educandoweb.projeto.entities.User;
 import com.educandoweb.projeto.repositories.OrderRepository;
-import com.educandoweb.projeto.repositories.UserRepository;
 
 
 
