979527a3f390fc78dd07726e546552c976b28de5
akiojapa <akioandrei@hotmail.com>
2022-03-08 09:01:15 -0300

    Exception handling - delete Checklist:
    Foi implementado o tratamento de exceções que podem ocorrer durante o procedimento do método Delete.

--- src/main/java/com/educandoweb/projeto/services/UserService.java
+++ src/main/java/com/educandoweb/projeto/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.projeto.entities.User;
 import com.educandoweb.projeto.repositories.UserRepository;
+import com.educandoweb.projeto.resources.exceptions.DatabaseException;
 import com.educandoweb.projeto.services.exception.ResourceNotFoundException;
 
 
@@ -33,7 +36,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
